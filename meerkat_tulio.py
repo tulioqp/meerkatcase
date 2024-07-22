ba218e6b0689f4d930342f7ecbcad3b2d5bae0ce
Túlio Queiroz <qptulio@gmail.com>
2024-07-22 11:02:29 -0300

    first commit

---
+++ meerkat_tulio.py
@@ -0,0 +1,49 @@
+import requests
+import streamlit as st
+from bs4 import BeautifulSoup
+
+
+# function to perform web scraping
+def scrape():
+    '''
+    inputs: void; outputs: the content from 'http://bianca.com'
+    '''
+
+    url = 'http://bianca.com'
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content = soup.find('h1')
+    if content:
+        return content.text.strip()
+    else:
+        return 'Content not found'
+    
+
+# aesthetic purpose only
+def beautify():
+    '''
+    inputs: void; outputs: '\n' 4 times, returns None
+    '''
+
+    for i in range(4):
+        st.text("\n")
+    
+    return None
+            
+
+# streamlit code
+st.html("<h1 style='text-align: center; color: violet;'>Case Meerkat</h1>")
+st.html("<h4 style='text-align: center; color: white;'><i>Web Scraping</i></h4>")
+st.header("", divider='violet')
+beautify()
+
+button = st.button('Realizar Web Scraping', type='primary', help='Clique para realizar o web scraping')
+if button:
+    with st.container(border=True):
+        st.title(scrape(), anchor=False)
+        st.write(':heart:\n')
+        button = st.button("Fechar", help='Clique para fechar')
+    st.balloons()
+else:
+    st.write('Clique para exibir o conteúdo')
